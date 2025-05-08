5258ef409de18857319c4be555fa4102b6a47fdc
mahmudrkb <almahmudrkb98@gmail.com>
2025-05-08 16:44:49 +0600

    solve 2 num question

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
 function formatString(input: string, toUpper?: boolean): string {
   if (toUpper === false) {
     return input.toLocaleLowerCase();
+  } else {
+    return input.toLocaleUpperCase();
   }
-  else {
-    return input.toLocaleUpperCase()
-  }
- 
 }
 // console.log(formatString("ami", true));
-console.log(formatString("Hello", ));
+console.log(formatString("Hello"));
 console.log(formatString("Hello", true));
 console.log(formatString("Hello", false));
-
 
 // Output: "HELLO"
 // Output: "HELLO"
 // Output: "hello"
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  let output = items.filter((item) => item.rating >= 4);
+  return output;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+//   { title: "Book s", rating: 6 },
+];
+
+console.log(filterByRating(books));
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
